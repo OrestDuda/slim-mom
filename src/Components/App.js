35962726d51fc019c7676d036ae7cd2cf1cf967d
Di-ua <di.ua@mail.ru>
2021-07-25 01:53:34 +0300

    Add loader and font GothamPro

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// import Loader from "./Components/Loader/Loader"
 
 function App() {
   return <></>;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./Components/App";
+import "./assets/fonts/fonts.scss"
 
 ReactDOM.render(
   <React.StrictMode>
