48a22d7b49b43d4be34085b7f70e811864a977e5
Orest <Orestduda@gmail.com>
2021-07-28 21:18:32 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import "./assets/fonts/fonts.scss"
 import App from './Components/App';
 import './services/baseURL';
 import { PersistGate } from 'redux-persist/integration/react';
