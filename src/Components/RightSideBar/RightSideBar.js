4723eeee10b55116625173949dd42ba519bfb7ab
Orest <Orestduda@gmail.com>
2021-08-03 01:39:49 +0300

    sidebar

--- src/Components/RightSideBar/RightSideBar.js
+++ src/Components/RightSideBar/RightSideBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import styles from './RightSideBar.module.scss';
 import userSelectors from '../../redux/user/userSelectors';
@@ -11,7 +11,7 @@ export default function RightSideBar() {
   const forday = useSelector(mealsSelectors.getDate);
 
   let sumKcal = 0;
-  const mealsKkal = mealsOnDay.map(item => {
+  mealsOnDay.map(item => {
     Number(item.kcal);
     sumKcal += item.kcal;
   });
@@ -23,45 +23,49 @@ export default function RightSideBar() {
   const notRecommendedFormattedString = notRecommendedFormatted.join(', ');
 
   return (
-    <div className={styles.container_Siderbar}>
-      <div className={styles.summary}>
-        <h2 className={styles.title}>Сводка за {forday}</h2>
-        <div className={styles.statistics}>
-          <ul className={styles.listName}>
-            <li className={styles.text}>
-              <span className={styles.text}>Осталось</span>
-              <span className={styles.text}>
-                {dailyLimit ? `${Math.round(leftForDay)} ккал` : '000 ккал'}
-              </span>
-            </li>
-            <li className={styles.text}>
-              <span className={styles.text}>Употреблено</span>
-              <span className={styles.text}>
-                {mealsOnDay.length === 0
-                  ? '000 ккал'
-                  : `${Math.round(sumKcal)} ккал`}
-              </span>
-            </li>
-            <li className={styles.text}>
-              <span className={styles.text}>Дневная норма</span>
-              <span className={styles.text}>
-                {dailyLimit ? `${Math.round(dailyLimit)} ккал` : '000 ккал'}
-              </span>
-            </li>
-            <li className={styles.text}>
-              <span className={styles.text}>n% от нормы</span>
-              {dailyLimit ? `${percentConsumed.toFixed(0)} %` : '000 ккал'}
-            </li>
-          </ul>
+    <div className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.summary}>
+          <h2 className={styles.title}>Сводка за {forday}</h2>
+          <div className={styles.statistics}>
+            <ul className={styles.listName}>
+              <li className={styles.list_wrap}>
+                <span className={styles.text}>Осталось</span>
+                <span className={styles.text}>
+                  {dailyLimit ? `${Math.round(leftForDay)} ккал` : '000 ккал'}
+                </span>
+              </li>
+              <li className={styles.list_wrap}>
+                <span className={styles.text}>Употреблено</span>
+                <span className={styles.text}>
+                  {mealsOnDay.length === 0
+                    ? '000 ккал'
+                    : `${Math.round(sumKcal)} ккал`}
+                </span>
+              </li>
+              <li className={styles.list_wrap}>
+                <span className={styles.text}>Дневная норма</span>
+                <span className={styles.text}>
+                  {dailyLimit ? `${Math.round(dailyLimit)} ккал` : '000 ккал'}
+                </span>
+              </li>
+              <li className={styles.list_wrap}>
+                <span className={styles.text}>n% от нормы</span>
+                <span className={styles.text}>
+                  {dailyLimit ? `${percentConsumed.toFixed(0)} %` : '000 ккал'}
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className={styles.menu}>
-        <h2 className={styles.title}>Нерекомендуемые продукты</h2>
-        <span className={styles.text}>
-          {notRecommended.length === 0
-            ? 'Здесь будет отображаться Ваш рацион'
-            : notRecommendedFormattedString}
-        </span>
+        <div className={styles.menu}>
+          <h2 className={styles.title}>Нерекомендуемые продукты</h2>
+          <span className={styles.text}>
+            {notRecommended.length === 0
+              ? 'Здесь будет отображаться Ваш рацион'
+              : notRecommendedFormattedString}
+          </span>
+        </div>
       </div>
     </div>
   );
