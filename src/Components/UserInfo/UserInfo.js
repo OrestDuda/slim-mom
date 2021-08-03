bfb92cdb69623871357f7b031020f1a76ac06965
Kateryna Boikova <kateboikova@gmail.com>
2021-08-03 09:29:11 +0200

    fix1.3.1

--- src/Components/UserInfo/UserInfo.js
+++ src/Components/UserInfo/UserInfo.js
@@ -1,26 +1,39 @@
-import styles from "./UserInfo.module.scss";
+import styles from './UserInfo.module.scss';
 import React from 'react';
 import userSelectors from '../../redux/user/userSelectors';
-import userOperations from "../../redux/user/userOperations";
-import {useSelector, useDispatch} from "react-redux";
+import userOperations from '../../redux/user/userOperations';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import useViewport from '../DiaryAddProductForm/helperDailAdd';
 
+export default function UserInfo() {
+  const user = useSelector(userSelectors.getUser);
+  const dispatch = useDispatch();
+  const userName = user.name;
 
-export default function UserInfo(){
-    const user = useSelector(userSelectors.getUser);
-    const dispatch = useDispatch();
-    const userName = user.name;
-    return(
-        <div className={styles.userBox}>
-            <div className={styles.userInfo}>
-                <span className={styles.name}>{userName}</span>
-                <span className={styles.slash}/>
-                <button type="button" className={styles.logout}  onClick={() => dispatch(userOperations.logOut())}>
-                    Выйти
-                </button>
-            </div>
-        </div>
-    )
+  const { width } = useViewport();
+  const breakpoint = 767;
+  const history = useHistory();
+  const handleGoBack = () => {
+    history.push('/dairy');
+  };
+  const location = useLocation();
+  return (
+    <div className={styles.userBox}>
+      {width < breakpoint && location.pathname === '/dairy/add' && (
+        <button onClick={handleGoBack}>back</button>
+      )}
+      <div className={styles.userInfo}>
+        <span className={styles.name}>{userName}</span>
+        <span className={styles.slash} />
+        <button
+          type="button"
+          className={styles.logout}
+          onClick={() => dispatch(userOperations.logOut())}
+        >
+          Выйти
+        </button>
+      </div>
+    </div>
+  );
 }
-
-
-
